cce2a7e7e46d71843f3c4f650cdeea5f0939aa42
jabe1204 <tjabet12@gmail.com>
2019-11-26 07:21:43 +0000

    Edición

--- ejercicio30.cpp
+++ ejercicio30.cpp
@@ -4,53 +4,70 @@
 
 using namespace std;
 
-void Funcion(double s, double D, int Nx, int Nt,string name);
+void ft(int Nx,int s,string name);
 	
 const int D = 1;
-const int s = 1;
+const int S = 1;
 const int Nx = 30;
-const int Nt = 15;
 const double dx = 2.0/Nx;
-const double dt = 2E-3;
+const int Nt = (2*D)/(dx*dx);
 
-int main(void)
+int main()
 {
-	Funcion(s,D,Nx,Nt,"datos.dat");
+	ft(Nx, S, "datos.dat");
 	return 0;
 }
 
-void Funcion(double s, double D, int Nx, int Nt, string name)
+void ft(int Nx, int s,string name)
 {
-	double M[Nx][Nt];
-    for(int i = 0; i < Nt; i++)
+	double N[Nt][Nx];
+	for(int i = 0; i < Nt; i++)
     {
-        for(int j = 0; j < Nx; j++)
+        for(int j = 0; j <=Nx; j++)
         {
 			if(i == 0 | j == 0 | j == Nx)
             {
-				M[i][j] = 0;
+				N[j][i] = 0;
 			}
 		}
 	}
 	
-	for(int i = 0; i < Nt; i++)
-    {
-		for(int j = 1; j < Nx; j++)
-        {
-			M[i+1][j] = M[i][j] + ((D*dt)/(pow(dx,2)))*(M[i][j+1] - 2*M[i][j] + M[i][j-1]) + dt*s;
-		}
+	for(int i = 0; i<Nt;i++)
+	{
+		for(int j = 0; j<Nx; j++)
+		{
+			N[j][i+1]= (N[j+1][i] + N[j-1][i])/2 + s/Nt;
+		}	
 	}
 	
 	ofstream outfile;
     outfile.open(name);
 	
-	for(int i = 0; i <= Nt; i++)
+	for(int i = 0; i < Nt; i++)
 	{
-		for(int j = 0; j <= Nx; j++)
+		for(int j = 0; j < Nx; j++)
 		{
-			outfile << M[i][j] << "\t";
+			outfile << N[j][i] << "\t";
 		}
 		outfile << endl;
 	}		
 	outfile.close();
 }
+
+//void ft2(double I,int nx)
+//{
+    //double P[Nt][nx];
+	
+    //for(int j=0; j<=Nx; j++)
+	//{    
+        //P[j][0] = 0;
+    //}
+    
+    //for(int i=1; i<=Nt; i++)
+	//{
+        //for(int j=0; j<Nx; j++)
+		//{
+            //P[j][i] = abs(I[j][i]-I[j-1][i]);
+        //}
+    //}
+//}
